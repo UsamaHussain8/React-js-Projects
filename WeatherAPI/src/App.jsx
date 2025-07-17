a2b7f935103e32d9be2a6e9838c1ef44fb0d9df2
UsamaHussain8 <usamahussain00@gmail.com>
2025-07-17 22:43:33 +0500

    Implemented API requests to the geolocation and weather API upon entering the city

--- WeatherAPI/src/App.jsx
+++ WeatherAPI/src/App.jsx
@@ -1,13 +1,100 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_KEY = "4fa3df17af0c3d7ee79623f2fb00b7ce"
+
+  const [city, setCity] = useState("")
+  const [lat, setLat] = useState(null)
+  const [long, setLong] = useState(null)
+
+  useEffect(() => {
+    if (city.trim() === "") return;
+
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      if (data.length > 0) {
+        setLat(data[0].lat)
+        setLong(data[0].lon)
+        console.log({lat: lat, long: long})
+        return ({lat: lat, long: long}) 
+      }
+      else {
+        console.log("No results found for the city you entered...")
+        setLat(null)
+        setLong(null)
+      }
+    })
+    .catch((err) => {
+      console.log(err) 
+      setLat(null)
+      setLong(null)
+    })
+  }, [city, setCity])
+
+  useEffect(() => {
+    if(city.trim() === '')  return;
+
+    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly&appid=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.length > 0) {
+        console.log({temp: data.current.temp, feels_like_temp: data.current.feels_like, humidity: data.current.humidity, wind_speed:data.current.wind_speed})
+        return ({lat: lat, long: long}) 
+      }
+      else {
+        console.log(`Sorry...Weather details could not be fetched for ${city}...`)
+      }
+    })
+    .catch((error) => {
+      console.log(error) 
+    })
+    }, [lat, long])
+
+  const handleCityKeyDown = (event) => {
+    if(event.key === "Enter") {
+      const cityName = event.target.value.trim();
+      console.log(cityName)
+      setCity(cityName)
+    }
+  }
+
+  // function getCityCoordinates() {
+  //   fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`)
+  //   .then((response) => {
+  //     return response.json()
+  //   })
+  //   .then((data) => {
+  //     setLat(data.lat)
+  //     setLong(data.long)
+  //     return ({lat: data.lat, long: data.long}) 
+  //   })
+  // }
 
   return (
     <>
       <h2 className="text-3xl">Weather Forecast</h2>
-      <h3 className="text-2xl bold">Islamabad</h3>
+      <label htmlFor="city" className="block my-4 text-lg/6 font-medium text-white-900">
+        Enter City Name
+      </label>
+      <input
+            id="city"
+            name="city"
+            type="text"
+            placeholder="City"
+            className="block min-w-0 grow my-2 py-4 pr-3 pl-1 text-base text-white-900 placeholder:text-gray-400 focus:outline-2 rounded-xl"
+            onKeyDown={handleCityKeyDown}
+      />
+
+      {/* Debug info - remove this later */}
+      <div className="mt-4 text-sm text-red-600">
+        <p>Current city: {city}</p>
+        <p>Latitude: {lat}</p>
+        <p>Longitude: {long}</p>
+      </div>
     </>
   )
 }
