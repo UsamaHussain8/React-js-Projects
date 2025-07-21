617c202f8e3853f7cf642372d5f80f0e0399efe6
UsamaHussain8 <usamahussain00@gmail.com>
2025-07-21 23:36:49 +0500

    Added weather component, processing for current and next 4 days weather

--- WeatherAPI/src/App.jsx
+++ WeatherAPI/src/App.jsx
@@ -1,73 +1,47 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import Weather from './components/Weather.jsx'
+
+const GEOLOCATION_API_KEY = import.meta.env.VITE_GEOLOCATION_API_KEY
 
 function App() {
-  const GEOLOCATION_API_KEY = import.meta.env.VITE_GEOLOCATION_API_KEY
-  const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY
-
   const [city, setCity] = useState("")
   const [lat, setLat] = useState(null)
   const [long, setLong] = useState(null)
 
   useEffect(() => {
-    if (city.trim() === "") return;
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${GEOLOCATION_API_KEY}`)
-    .then((response) => {
-      return response.json()
-    })
-    .then((data) => {
-      if (data.length > 0) {
-        const latitude = data[0].lat
-        const longitude = data[0].lon
-        setLat(latitude)
-        setLong(longitude)
-        return ({lat: latitude, long: longitude}) 
-      }
-      else {
-        console.log("No results found for the city you entered...")
-        setLat(null)
-        setLong(null)
-      }
-    })
-    .catch((err) => {
-      console.log(err) 
-      setLat(null)
-      setLong(null)
-    })
-  }, [city, setCity])
-
-  useEffect(() => {
-    if(city.trim() === '')  return;
-
-    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely&appid=${WEATHER_API_KEY}`)
-    .then((response) =>  {
-      console.log(response)
-      return response.json()
-  })
-    .then((data) => {
-      if (data) {
-        console.log(data)
-        const sunriseTime = new Date(data.current.sunrise * 1000)
-        const temp_celsius = Math.round((data.current.temp - 273.15) * 100.0) / 100;
-        const feels_like_temp_celsius = Math.round((data.current.feels_like - 273.15) * 100.0) / 100;
-        console.log({sunrise: sunriseTime.toLocaleTimeString(), temp: temp_celsius, feels_like_temp: feels_like_temp_celsius, 
-                    humidity: data.current.humidity, wind_speed:data.current.wind_speed, description: data.current.weather[0].description})
-      }
-      else {
-        console.log(`Sorry...Weather details could not be fetched for ${city}...`)
-      }
-    })
-    .catch((error) => {
-      console.log(error) 
-    })
-    }, [lat, long])
+          if (city.trim() === "") return;
+          fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${GEOLOCATION_API_KEY}`)
+          .then((response) => {
+          return response.json()
+          })
+          .then((data) => {
+            if (data.length > 0) {
+              const latitude = data[0].lat
+              const longitude = data[0].lon
+              setLat(latitude)
+              setLong(longitude)
+              return ({lat: latitude, long: longitude}) 
+            }
+            else {
+              console.log("No results found for the city you entered...")
+              setLat(null)
+              setLong(null)            
+            }
+          })
+          .catch((err) => {
+            console.log(err) 
+            setLat(null)
+            setLong(null)
+          })
+      }, [city, setCity])
 
   const handleCityKeyDown = (event) => {
-    if(event.key === "Enter") {
-      const cityName = event.target.value.trim();
-      console.log(cityName)
-      setCity(cityName)
-    }
+        if(event.key === "Enter") {
+          const cityName = event.target.value.trim();
+          console.log(cityName)
+          setCity(cityName)
+        }
   }
 
   return (
@@ -83,14 +57,12 @@ function App() {
             placeholder="City"
             className="block min-w-0 grow my-2 py-4 pr-3 pl-1 text-base text-white-900 placeholder:text-gray-400 focus:outline-2 rounded-xl"
             onKeyDown={handleCityKeyDown}
-      />
+        />
 
-      {/* Debug info - remove this later */}
-      <div className="mt-4 text-sm text-red-600">
-        <p>Current city: {city}</p>
-        <p>Latitude: {lat}</p>
-        <p>Longitude: {long}</p>
-      </div>
+      <Weather city={city}
+               lat={lat}
+               long={long}
+      />
     </>
   )
 }
---
+++ WeatherAPI/src/components/Weather.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from "react";
+
+const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY
+
+function Weather({ city, lat, long }) {
+    const [weatherData, setWeatherData] = useState({
+    current: null,
+    daily: []
+  });
+
+    useEffect(() => {
+        if(city.trim() === '')  return;
+
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude=hourly,minutely&appid=${WEATHER_API_KEY}`)
+        .then((response) =>  {
+            console.log(response)
+            return response.json()
+        })
+        .then((data) => {
+            if (data) {
+                console.log(data)
+                const sunriseTime = new Date(data.current.sunrise * 1000)
+                const temp_celsius = Math.round((data.current.temp - 273.15) * 100.0) / 100;
+                const feels_like_temp_celsius = Math.round((data.current.feels_like - 273.15) * 100.0) / 100;
+                const processedCurrentWeather = {
+                    sunrise: sunriseTime.toLocaleTimeString(), 
+                    temp: temp_celsius, 
+                    feels_like_temp: feels_like_temp_celsius, 
+                    humidity: data.current.humidity, 
+                    wind_speed:data.current.wind_speed, 
+                    description: data.current.weather[0].description,
+                    icon: data.current.weather[0].icon,
+                };
+                
+                const processedWeatherForecast = data.daily.slice(0, 4).map(day => ({
+                    date: new Date(day.dt * 1000).toLocaleDateString(),
+                    sunrise: new Date(day.sunrise * 1000).toLocaleTimeString(),
+                    temp: Math.round( (day.temp.max - 273.15) * 100.0) / 100.0,
+                    feels_like_temp: Math.round((day.feels_like.day - 273.15) * 100.0) / 100.0,
+                    humidity: day.humidity,
+                    wind_speed: day.wind_speed,
+                    description: day.weather[0].description,
+                    icon: day.weather[0].icon,
+                }));
+                console.log(processedWeatherForecast)
+
+                setWeatherData({
+                    current: processedCurrentWeather,
+                    daily: processedWeatherForecast,
+                })
+            }
+            else {
+                console.log(`Sorry...Weather details could not be fetched for ${city}...`)
+            }
+        })
+        .catch((error) => {
+            console.log(error) 
+        })
+    }, [lat, long, city])
+
+    if (!city) return <div> Please enter the name of the city</div>
+    if (!weatherData.current) return <div>No weather data available</div>;
+
+    return (
+    <>
+        <div className="current-weather">
+            <h2>Current Weather in {city}</h2>
+            <p>Temperature: {weatherData.current.temp}°C</p>
+            <p>Feels like: {weatherData.current.feels_like_temp}°C</p>
+            <p>Description: {weatherData.current.description}</p>
+            <p>Humidity: {weatherData.current.humidity}%</p>
+            <p>Wind Speed: {weatherData.current.wind_speed} m/s</p>
+            <img 
+                src={`https://openweathermap.org/img/wn/${weatherData.current.icon}@2x.png`}
+                alt={weatherData.current.description}
+            />
+        </div>
+    </>
+  )
+}
+
+export default Weather
